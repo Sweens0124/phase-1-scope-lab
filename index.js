eed46eb33ccfffe7fe1477279ef5417c77a4b7ff
Lucas Sweeney <lsweeney0124@gmail.com>
2022-05-19 09:54:12 -0600

    commit tree dddea2823b013a2490e3435fecdf16ee979307d0

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName () {
+  return customerName = customerName.toUpperCase()
+}
+
+// write function setBestCustomer() that WHEN CALLED, the function declares a variable called "bestCustomer" which is in global scope, then assigns that variable to 'not bob'.....
+function setBestCustomer () {
+  bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer () {
+  bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'lucas'
+function changeLeastFavoriteCustomer () {
+  leastFavoriteCustomer = 'lucas'
+}
